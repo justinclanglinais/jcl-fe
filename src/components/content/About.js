5b4c3a5f03e5c4b103146d8d899c269ada94878f
Justin Langlinais <justinclanglinais@gmail.com>
2020-11-09 10:33:38 -0800

    feat(About): testing CSS and layout

--- src/components/content/About.js
+++ src/components/content/About.js
@@ -3,8 +3,15 @@ import React from 'react'
 export default class About extends React.Component {
     render () {
         return (
-            <div className="App-content">
-                <h1>About me</h1>
+            <div className="Content-container">
+                <div className="Content-title">
+                    <h1>About me</h1>
+                </div>
+                <div>
+                    <p>
+                        I grew up in Oklahoma and I did other stuff and I skateboarded and I did car motor swaps and I have done jiu jitsu for a long time, I was a designer video editor I love to cook I love dogs I love Lucy 
+                    </p>
+                </div>
             </div>
         )
     }
